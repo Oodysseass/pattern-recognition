492c907561e0e6d481b4c76cfa33a6a6d540a5d4
Odysseas Sofikitis <o.sofikitis@gmail.com>
2024-01-02 16:00:15 +0200

    Makes data loading more robust

--- data.py
+++ data.py
@@ -4,13 +4,12 @@ from sklearn.model_selection import train_test_split
 
 def load_data(filename, size):
     ## load data
-    custom_columns = ['feature 1', 'feature 2', 'label']
-    data_file = pd.read_csv(filename, header=None, names=custom_columns)
+    data_file = pd.read_csv(filename, header=None)
     data_file = data_file.dropna()
     
     # make train and test sets
-    data_x = np.array(data_file.drop('label', axis=1))
-    data_y = np.array(data_file['label'])
+    data_x = np.array(data_file.drop(data_file.columns[-1], axis=1))
+    data_y = np.array(data_file[data_file.columns[-1]])
     
     train_x, test_x, train_y, test_y = train_test_split(data_x, data_y,
                                                         test_size=size,
