5e58caa361def7daaf30514f3014ec0a9ab912bd
Odysseas Sofikitis <o.sofikitis@gmail.com>
2024-01-03 16:31:44 +0200

    data.py

--- data.py
+++ data.py
@@ -6,11 +6,11 @@ def load_data(filename, size):
     ## load data
     data_file = pd.read_csv(filename, header=None)
     data_file = data_file.dropna()
-    
+
     # make train and test sets
     data_x = np.array(data_file.drop(data_file.columns[-1], axis=1))
     data_y = np.array(data_file[data_file.columns[-1]])
-    
+
     train_x, test_x, train_y, test_y = train_test_split(data_x, data_y,
                                                         test_size=size,
                                                         stratify=data_y,
